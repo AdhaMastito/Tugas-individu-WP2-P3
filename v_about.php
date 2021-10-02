a14b82ed21cf18884621b171badabe0016bd0b71
Adha Mastito <90718945+AdhaMastito@users.noreply.github.com>
2021-10-02 18:13:40 +0700

    Upload Tugas Pertemuan 3 WP2

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+ 	<h4>Nama</h4>
+ 	<ul type="disc">
+ 		<li>Nama Depan : Adha</li>
+ 		<li>Nama Belakang : Mastito</li>
+ 		<li>Nim : 19200914</li>
+ 	</ul>
+ <br>
+ 	<h4>Alamat</h4>
+ 	<ul type="none">
+ 		<li> Jln.Pulo Cempaka 3 no.40 Rt/Rw 006/016</li>
+ 	</ul>
+ 
+ 	<h4>Tempat Lahir</h4>
+ 	<ul type="none">
+ 		<li>Jakarta</li>
+ 	</ul>
+ 	<h4>Olahraga Favorite</h4>
+ 	<ul type="square">
+ 		<li>Sepak Bola</li>
+ 		<li>Berenang</li>
+ 	</ul>
+</section>
